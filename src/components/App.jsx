3824f13027728755298c57f1e9b783650dbd7c2a
GGalina <71042425+GGalina@users.noreply.github.com>
2023-04-01 00:06:07 +0400

    HW completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,21 @@ import { Container, Title, SubTitle } from './Phonebook.styled';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
+  };
+
+  componentDidMount() {
+    const contactsStored = JSON.parse(localStorage.getItem('contacts'));
+    if (contactsStored) {
+      this.setState({ contacts: contactsStored });
+    }
+  };
+
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
   };
 
   handleAddItem = event => {
